76846fb9d273c4d9fd5787a5c33ce913f631dd50
AndriyNikitchin <nikitchinandriy@gmail.com>
2020-04-09 19:17:16 +0300

    hw2-task1

---
+++ hw2/task1.js
@@ -0,0 +1,75 @@
+"use strict"
+// let fruits = ["яблоки", "бананы", "апельсины"];
+
+// console.log(fruits);
+// console.log(fruits[1]);
+
+// fruits[1] = "киви";
+// console.log(fruits);
+
+// fruits[3] =  "груши";
+// fruits[4] =  "виноград";
+// fruits[5] =  "персики";
+
+// console.log(fruits);   
+//1=done
+
+///////////////////////////////////////
+
+// let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
+// const score1 = 12;
+
+// for (let a = 0; a < test1.length; a++){
+//     if(test1[a] <= score1){
+//         continue
+//     }
+//     console.log(test1[a]);
+// }
+
+//2=done
+
+/////////////////////////////////////
+
+// let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
+// const score2 = 15;
+
+// for (let b = 0; b < test2.length; b++){
+//     if(test2[b] <= score2){
+//         continue
+//     }
+//     console.log(test2[b]);
+// }
+
+//3=done
+
+////////////////////////////////////
+
+
+// let test3 =[20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
+// const score3 = 16;
+
+// for (let с = 0; с < test3.length; с++){
+//     if(test3[с] <= score3){
+//         continue;
+//     }
+//     console.log(test3[с]);
+// }
+
+//4=done
+
+//////////////////////////////////
+
+// let tests = [
+//     [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12],
+//     [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20],
+//     [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22]
+// ];
+
+// for (let a = 0;a < tests.length; a+=1) {
+//   console.log( tests[a] );
+//   for(let c = 0; c < tests.length; c+=1){
+//       console.log(tests[a][c]);
+//   }
+// }
+
+//5=done
---
+++ hw3/index.js
@@ -0,0 +1,169 @@
+"use strict"
+
+// // const object = {
+// //     key: value,
+// //     key: value   
+// }
+// const frontend = {
+//     language_1: "HTML",
+//     language_2: "CSS",
+//     language_3: "Js"
+// };
+// console.log("frontend", frontend);
+// const student = {
+// name: "Alksandra",
+// age: 31,
+// sex: "female",
+// course: frontend,
+// passExam: true 
+// };
+// console.log("student", student);
+
+// console.log(student.name);
+// console.log(student.lastName);
+// console.log(student.age);
+// console.log(student.sex);
+// console.log(student.course);
+// console.log(student.paaExam);
+
+// console.log(frontend["language_1"]);
+// console.log(frontend["language_2"]);
+// console.log(frontend["language_3"]);
+
+// student.lastName = "0'Hurn";
+// console.log(student.lastName);
+
+// console.log("student", student);
+
+// frontend["language_3"] = "Javascript";
+// console.log(frontend["language_3"]);
+
+// console.log("frontend", frontend);
+
+// student.address = "Heroyv Staluhradu, 6";
+// console.log(student.address);
+
+// console.log("student", student);
+
+// delete frontend.lanfuage_1;
+// console.log(frontend.language_1);
+// console.log("frontend", frontend);
+
+// delete frontend["lanfuage_2"];
+// console.log(frontend["language_2"]);
+// console.log("frontend", frontend);
+
+// const myTeacher = "Sasha Hrevtsove";
+// const course = "frontend";
+
+// const group_17 = {
+//     myTeacher,                             
+//     course,
+//     count: 10
+// };
+// console.log(group_17);
+
+// const key = "test";
+
+// const getKey = function() {
+//     return "score";
+// };
+
+// const testResult = {
+//     key,
+//     [key]: "2",
+//     [getKey()]: 22
+// };
+
+// console.log(testResult);
+
+/////////////////////////////////
+
+// const myProducts = {
+//     milk: 0.5,
+//     eggs: 10,
+//     apples: 5,
+//     toCheck() {
+//         console.log("Проверь наличие");
+//     },
+//     oranges: 10
+// };
+// myProducts.toCheck();
+// console.log("myProducts", myProducts);
+
+// myProducts.toBuySweets = function() {
+// console.log("Ну купи уже что-то сладенькое!");
+// };
+
+// myProducts.toBuySweets();
+// console.log("myProducts with new method", myProducts);
+
+// delete myProducts.toBuySweets;
+// console.log("myProducts without new method", myProducts);
+
+///////////////////////////////////////////////////
+
+// const product = {
+//     type: "welness & spa",
+//     category: "hair care",
+//     productName: "shampoo",
+//     price: 76.94,
+//     showProductName() {
+//         console.log(this.productName);
+//     },
+//     changePrice(value) {
+//         this.price = value;
+//     }
+
+// };
+
+// product.showProductName();
+
+// product.changePrice(89.5);
+// console.log(product.price);
+
+////////////////////////////////////////////////////
+
+// const myCars = {
+//     tesla: "modelX",
+//     zhiguli: "kopeyka",
+//     lada: "xRay",
+//     bmx:  "mongoose"
+// };
+
+// for (const key in myCars) {
+//  console.log("key: ", key);
+// }
+
+// for (const key in myCars) {
+//     console.log("value: ", myCars[key]);
+// }
+
+///////////////////////////////////////////////////
+
+const hero = {
+    name: "Black cat",
+    power: 100,
+    lifes: 9,
+    speed: 45
+};
+
+console.log("my hero", hero);
+
+const keysOfHero = Object.keys(hero);
+console.log(keysOfHero);
+
+for (const key of keysOfHero) {
+console.log("key: ",key);
+console.log("value: ", hero[key]);
+}
+
+const entriesOfHero = Object.entries(hero);
+console.log("entries Of Hero", entriesOfHero);
+
+for (const entry of entriesOfHero) {
+    const key = entry[0];
+    const value = entry[1];
+
+    console.log('${key}: ${value}')
+}
