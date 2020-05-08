f0bb29cb1e5ae19ec78d592fff219386fb7d2eab
AndriyNikitchin <nikitchinandriy@gmail.com>
2020-05-08 23:47:13 +0300

    nugo

--- mod2/task3.js
+++ mod2/task3.js
@@ -1,9 +1,121 @@
 "use strict"
 
-let toPassTests = function() {
-    alert("Прежде, чем выполнить это задание у тебя должно быть сдано пять тестов!");
-}
+// let toPassTests = function() {
+//     alert("Прежде, чем выполнить это задание у тебя должно быть сдано пять тестов!");
+// }
 
-let toPassTests = () => alert("Прежде, чем выполнить это задание у тебя должно быть сдано пять тестов!");
+// let toPassTests = () => alert("Прежде, чем выполнить это задание у тебя должно быть сдано пять тестов!");
 
-toPassTests();
+// toPassTests();
+
+//1-done
+
+//////////////////////////////////////////////////////////////////
+
+// let age = prompt("Enter your age");
+
+//////////////////////////
+
+// let checkAge = function(){
+//     if(age >= 18){
+//         alert("Here you are! You are got access!");
+//     } 
+//     else{
+//         alert("Sorry, you can't to get access");
+//     }
+// }
+
+////////////////////////////
+
+
+// let checkAge = () => (age >= 18) ? alert("Here you are! You are got access!") : alert("Sorry, you can't to get access");
+
+// checkAge();
+
+//2-done
+
+//////////////////////////////////////////
+
+// let toGetCost = function(message, pricePerWord) {
+//     let countWords = message.split(" ").length;
+//        console.log(pricePerWord = countWords * +pricePerWord);
+// };
+
+//////////////////////////
+// let toGetCost = (message, pricePerWord) => (pricePerWord = message.split(" ").length* pricePerWord);
+
+// console.log(toGetCost("To be, or not to be, that is the question", 10)); // 100
+// console.log(toGetCost("Whether 'tis nobler in the mind to suffer", 20)); // 160
+// console.log(toGetCost("The slings and arrows of outrageous fortune,", 40)); // 280
+// console.log(toGetCost("Or to take arms against a sea of troubles", 20)); // 180
+
+//3-done
+
+///////////////////////////////////////////////
+
+// let logItems = function(array){
+//     for(let a = 0; a < array.length; a++){
+//         console.log(`${a+1} - ${array[a]}`);
+//     }
+// }
+
+// logItems([
+//     "selo",
+//     "ogorod",
+//     "tractor",
+//     "palka",
+//     "sobaka",
+//     "kot"
+//   ]);
+
+// logItems([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]);
+
+//4-done
+
+////////////////////////////////////////////////////
+
+
+// let checkTheWord = function(message) {
+//   let words = message.split(" ");
+//   if (words.includes("lose") || words.includes("not")) {
+//     console.log("true");
+//   }
+// }
+
+/////////////////
+
+// let checkTheWord = (message) => (message.split(" ").includes("lose") || message.split(" ").includes("not")) ?  true :  false;
+
+  
+// console.log(checkTheWord("Than fly to others that we know not of?")); // true
+// console.log(checkTheWord("Thus conscience does make cowards of us all")); // false
+// console.log(checkTheWord("And enterprises of great pitch and moment")); // false
+// console.log(checkTheWord("And lose the name of action")); // true
+
+//5-done
+
+///////////////////////////////////////////////
+
+// let totalSum = 0;
+// let countTestsScore = function(){
+//   for(let argument of arguments)
+    
+//     totalSum = totalSum + argument;
+//     console.log(totalSum)
+
+// }
+// countTestsScore(16, 22, 23); 
+// countTestsScore(15, 20, 22, 20); 
+// countTestsScore(14, 19, 21, 20, 23); 
+
+// 6-done
+
+//////////////////////////////////////////////////
+
+// let  myNewArrowFunction = (...rest) => console.log(rest);
+  
+// myNewArrowFunction(1, 2, 3); [1, 2, 3]
+// myNewArrowFunction(100, 200, 300, 400, 500); [100, 200, 300, 400, 500]
+// myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
+
+//7-done
